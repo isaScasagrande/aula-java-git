6c2b2163708d98f9fe9b4afffffd959a12e5a16f
05021377205 <05021377205@JIPA-LABIN03-29.ifro.local>
2018-03-07 14:55:50 -0400

    Criando a classe calculadora

---
+++ ProjetoGitJava/src/Calculadora.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 05021377205
+ */
+public class Calculadora {
+   
+    public double soma ( double num1, double num2 ){
+       return num1 + num2;
+       
+    }
+    
+    public double subtracao ( double num1 , double num2){
+        return num1 - num2 ;
+    }
+    
+    public double divisao ( double num1 , double num2 ){
+    return num1 / num2 ;
+      
+    }
+    public double multiplicacao ( double num1 , double num2 ){
+    return num1 * num2 ;
+    
+    }
